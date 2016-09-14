cbb746083380dfcb2971430f954770677df6275c
matt venn <matt@mattvenn.net>
2016-09-14 14:35:40 +0200

    args

---
+++ prussC/generate_hit.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import argparse
+samples_per_us = 0.2647
+mvolts_per_bit = 3420.0 / 256 # should be 3300 but this bbb measures at 3.42v
+
+parser = argparse.ArgumentParser(description="generate hit test data")
+parser.add_argument('--zero-val', action='store', type=int, help="zero value (mv)", default=0)
+parser.add_argument('--hit-max', action='store', type=int, help="max hit value (mv)", default=2000)
+parser.add_argument('--hit-len', action='store', type=int, help="hit length (us)", default=60)
+parser.add_argument('--hit-slope', action='store', type=int, help="slope of hit (mv/us)", default=100)
+
+args = parser.parse_args()
+
+# do conversions from us and mv to samples and bits
+args.zero_val /= mvolts_per_bit # mvolts converted to byte
+args.hit_max /= mvolts_per_bit # mvolts converted to byte
+args.hit_len /= samples_per_us # us converted to samples
+args.hit_slope /= (mvolts_per_bit/samples_per_us)
+
+
+val = args.zero_val
+samples = []
+
+while True:
+    if val < args.hit_max and len(samples) < args.hit_len:
+        val += args.hit_slope
+    elif len(samples) > args.hit_len:
+        val -= args.hit_slope
+        if val < args.zero_val:
+            break
+    samples.append(val)
+		
+# check signal is short enough
+assert len(samples) < 7500 
+with open("data.txt", 'w') as fh:
+    for val in samples:
+        fh.write("%d\n" % val)
+print("hit file generated, %d samples long" % len(samples))
